b0fe963e98bcbc9d15653839f6e6775b057776a4
buscaroli <m.buscaroli@outlook.com>
2023-01-17 15:23:51 +0000

    Initial Commit

---
+++ DogGram/DogGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogGramApp.swift
+//  DogGram
+//
+//  Created by Matteo on 17/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct DogGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
